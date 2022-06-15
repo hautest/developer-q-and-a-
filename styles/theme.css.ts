d24490734a0247d000963b4724adeec6cc627448
LeeSeungHun <dos07008@naver.com>
2022-06-16 00:15:29 +0900

    refactor : theme

--- styles/theme.css.ts
+++ styles/theme.css.ts
@@ -4,9 +4,10 @@ export const [themeClass, vars] = createTheme({
   colors: {
     white: "#FFFFFF",
     black: "#000000",
-    backgroundDarkMode: "#202124",
-    backgroundDarkModeNav: "#354259",
-    backgroundLightModeMain: "#ECE5C7",
+    color1: "#C2DED1",
+    color2: "#ECE5C7",
+    color3: "#CDC2AE",
+    color4: "#354259",
   },
   spacing: {
     sm: "8px",
