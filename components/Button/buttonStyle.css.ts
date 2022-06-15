45df98ed1d57b0fabe2e5a5c50b6303cc4edff0f
LeeSeungHun <dos07008@naver.com>
2022-06-16 00:15:19 +0900

    refactor: button style

--- components/Button/buttonStyle.css.ts
+++ components/Button/buttonStyle.css.ts
@@ -4,8 +4,13 @@ import { style } from "@vanilla-extract/css";
 import { vars } from "../../styles/theme.css";
 
 export const buttonStyle = style({
-  backgroundColor: vars.colors.backgroundDarkModeNav,
+  backgroundColor: vars.colors.color2,
   padding: vars.spacing.sm,
   border: "0",
   cursor: "pointer",
+  borderRadius: "20px",
+  width: "50%",
+  ":hover": {
+    backgroundColor: vars.colors.color3,
+  },
 });
