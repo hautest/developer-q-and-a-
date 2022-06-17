9715ed23277718ad692ea19304ce3f2afa7f5f03
LeeSeungHun <dos07008@naver.com>
2022-06-17 21:07:01 +0900

    refator: index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,25 @@
-import Link from "next/link";
+import { useRouter } from "next/router";
 
 import { homeStyle, titleStyle } from "../styles/index.css";
 import { themeClass } from "../styles/theme.css";
 import Button from "../components/Button/Button";
+import Link from "next/link";
 
 function Home() {
   return (
     <div className={`${homeStyle} ${themeClass} `}>
       <div className={titleStyle}>DEVELOPER Q&A</div>
-      <Button>시작하기</Button>
-      <Button>Q&A한번에보기</Button>
-      <Button>Q&A추가하기</Button>
-      <Button>설명서</Button>
+      <Link href="/quiz">
+        <a>
+          <Button size="lg">시작하기</Button>
+        </a>
+      </Link>
+      <Link href="/showAllQAndA">
+        <a>
+          <Button size="lg">Q&A한번에보기</Button>
+        </a>
+      </Link>
+      <Button size="lg">설명서</Button>
     </div>
   );
 }
